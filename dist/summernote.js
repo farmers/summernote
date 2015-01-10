168fa5b50c7ea6f02a3af20b7e75341f72556828
amondnet <amond@amond.net>
2015-01-10 16:06:45 +0900

    dropdown not working

--- dist/summernote.js
+++ dist/summernote.js
@@ -5020,7 +5020,7 @@
         trigger: 'hover',
         placement: sPlacement || 'top'
       }).on('click', function () {
-        $(this).tooltip('hide');
+        $(this).tooltip('option','hide');
       });
     };
 
